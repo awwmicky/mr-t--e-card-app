3d85fd9659e87bde44514a70a81aa7db18c8e000
Adnan Niaz <adnanniaz77@yahoo.com>
2020-04-03 14:13:14 -0700

    Adi changes

--- src/index.js
+++ src/index.js
@@ -1,52 +1,52 @@
 // import * as serviceWorker from './serviceWorker'
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom";
 import {
-  BrowserRouter as Router,
-  // HashRouter as Router,
-  Route , Switch
-} from 'react-router-dom'
-import React from 'react'
+    BrowserRouter as Router,
+    // HashRouter as Router,
+    Route,
+    Switch,
+} from "react-router-dom";
+import React from "react";
 
-import './assets/reset.css'
-import './assets/index.css'
+import "./assets/reset.css";
+import "./assets/index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // import ___ from './components/___/'
-import Header from './components/Header/'
+import Header from "./components/Header/";
 // import ___ from './views/StudentPage/___/___'
-import ErrorPage from './views/ErrorPage/'
-import HomePage from './views/HomePage/'
+import ErrorPage from "./views/ErrorPage/";
+import HomePage from "./views/HomePage/";
 
 /* ↓ import your component page here ↓ */
 import MichaelAlvarez from "./views/StudentPage/MichaelAlvarez/MichaelAlvarez";
+import Adi from "./components/Adi/Adi";
 // import MarioAlvarez from "./views/StudentPage/MarioAlvarez/MarioAlvarez";
 
+function App() {
+    return (
+        <>
+            <Router>
+                <Header />
 
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Adi />
+                    {/* ↓ add your route component here ↓ */}
+                    <Route
+                        exact
+                        path="/michael-alvarez"
+                        component={MichaelAlvarez}
+                    />
+                    {/* <Route exact path="/mario-alvarez" component={ MarioAlvarez } /> */}
 
-function App () {
-
-  return (
-    <>
-      <Router>
-        <Header />
-
-        <Switch>
-          <Route exact path="/" component={ HomePage } />
-
-          {/* ↓ add your route component here ↓ */}
-          <Route exact path="/michael-alvarez" component={ MichaelAlvarez } />
-          {/* <Route exact path="/mario-alvarez" component={ MarioAlvarez } /> */}
-
-          <Route path='*' component={ ErrorPage } />
-        </Switch>
-      </Router>
-    </>
-  );
-
+                    <Route path="*" component={ErrorPage} />
+                </Switch>
+            </Router>
+        </>
+    );
 }
 
-
-
-const root = document.getElementById('root');
-ReactDOM.render( <App /> , root )
+const root = document.getElementById("root");
+ReactDOM.render(<App />, root);
 // serviceWorker.unregister();
