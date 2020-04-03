2bc35ba0951d67bc575b35f75b09f1445dd15469
mariofalvarez <mariofalvarezjr@gmail.com>
2020-04-03 14:54:39 -0700

    testing

---
+++ src/views/StudentPage/MarioAlvarez/MarioAlvarez.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+// , { useState }
+
+import "./MarioAlvarez.css"
+
+export default function MarioAlvarez() {
+  return (
+    <main>
+      <h2> First & Last Name </h2>
+      <img
+        src="./assets/images/other/< your-profile.img >"
+        alt="student-portfolio"
+      />
+      <p>
+        -a memorable / funny moment with Thomas & the class - additional message
+        for Thomas(feedback / question)
+      </p>
+      <div>
+        <a href="" target="_blank" rel="noopener noreferrer" title="">
+          links to your GitHub, LinkedIn, etc.
+          <i className="fab fa-font-awesome"> </i>
+        </a>
+      </div>
+    </main>
+  )
+}
