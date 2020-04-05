1f477d514b70cbbc535a7a5fce1b99552572e10f
mariofalvarez <mariofalvarezjr@gmail.com>
2020-04-05 15:53:32 -0700

    small fix

--- src/views/StudentPage/MarioAlvarez/MarioAlvarez.js
+++ src/views/StudentPage/MarioAlvarez/MarioAlvarez.js
@@ -16,7 +16,7 @@ export default function MarioAlvarez() {
         surface.I 'm excited to keep learning and practicing to master my craft!
       </p>
       <div>
-        <a href="" target="_blank" rel="noopener noreferrer" title="">
+        <a href="/" target="_blank" rel="noopener noreferrer" title="">
           links to your GitHub, LinkedIn, etc.
           <i className="fab fa-font-awesome"> </i>
         </a>
