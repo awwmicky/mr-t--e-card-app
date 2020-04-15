048e8dc3620543c0a6bde5556b2ea0b923d3b22f
Larry-nan <lawrence,yinan@gmail.com>
2020-04-15 15:47:38 -0700

    larry update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 // import * as serviceWorker from './serviceWorker'
 import ReactDOM from "react-dom";
 import {
--- src/views/HomePage/index.js
+++ src/views/HomePage/index.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React
+// , { useState }
+from 'react'
 import './HomePage.css'
 
 
