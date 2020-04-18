efa315311b3b352bc73c6e58e41da2f577c92da6
mariofalvarez <mariofalvarezjr@gmail.com>
2020-04-18 16:32:12 -0700

    added critical changes

--- src/views/StudentPage/MarioAlvarez/MarioAlvarez.js
+++ src/views/StudentPage/MarioAlvarez/MarioAlvarez.js
@@ -2,46 +2,52 @@ import React from "react";
 
 export default function MarioAlvarez() {
   return (
-    <main>
-      <div className="jumbotron">
-        <h1 className="display-4">Mario Alvarez</h1>
+    <main className="d-flex justify-content-center flex-column border border-primary align-items-center">
+      <div className="card d-flex justify-content-center flex-column text-center col-md-4">
+        <div className="card-body pt-5">
+          <h1 className="display-4 card-title text-center">Mario Alvarez</h1>
 
-        <p className="lead">
-          Thanks for being an incredible mentor thoughout the beginning of my
-          coding journey. We learned so much and now I know we just barely
-          scratched the surface. I'm excited to keep practicing and learning to
-          master my craft. Cheers!
-        </p>
+          <img
+            src="./assets/images/profile-pic/mario.jpg"
+            alt="mario-profile"
+            className="img-fluid"
+          />
 
-        <img src="./assets/images/profile-pic/mario.jpg" alt="mario-profile" />
+          <p className="lead">
+            Thanks for being an incredible mentor thoughout the beginning of my
+            coding journey. We learned so much and now I know we just barely
+            scratched the surface. I'm excited to keep practicing and learning
+            to master my craft. Cheers!
+          </p>
 
-        <p className="lead">
-          <a
-            href="mailto:mariofalvarezjr@gmail.com"
-            title="Direct Email"
-            className="btn btn-primary btn-lg"
-          >
-            <i className="fas fa-envelope-square"></i>
-          </a>
-          <a
-            href="https://www.github.com/mariofalvarez"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="GitHub"
-            className="btn btn-primary btn-lg"
-          >
-            <i className="fab fa-github-square"></i>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/mariofalvarez/"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="LinkedIn"
-            className="btn btn-primary btn-lg"
-          >
-            <i className="fab fa-linkedin"></i>
-          </a>
-        </p>
+          <p className="lead">
+            <a
+              href="mailto:mariofalvarezjr@gmail.com"
+              title="Direct Email"
+              className="btn btn-primary btn-lg"
+            >
+              <i className="fas fa-envelope-square"></i>
+            </a>
+            <a
+              href="https://www.github.com/mariofalvarez"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="GitHub"
+              className="btn btn-primary btn-lg"
+            >
+              <i className="fab fa-github-square"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/mariofalvarez/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="LinkedIn"
+              className="btn btn-primary btn-lg"
+            >
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </p>
+        </div>
       </div>
     </main>
   );
