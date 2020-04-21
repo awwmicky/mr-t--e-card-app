9cb334fe7445a776b543cda519b3c8fe76030eab
laurabaoye <laurabaoye@gmail.com>
2020-04-20 19:04:53 -0700

    component update

--- src/views/StudentPage/LauraBao/LauraBao.js
+++ src/views/StudentPage/LauraBao/LauraBao.js
@@ -14,49 +14,49 @@
 import React from 'react'
 import './LauraBao.css'
 
-export default function LauraBao () {
+export default function LauraBao() {
 
   return (
     <main className="d-flex align-items-center justify-content-center">
       <div className="card">
         <img
           className="card-img card-img"
-          src="./assets/images/profile-pic/< your-profile-pic.img >"
+          src="./assets/images/profile-pic/laura.JPG"
           alt="student-profile-pic"
         />
         <div className="card-body">
           <h3 className="card-title font-weight-bold text-center">
-            Your First & Last Name
+            Laura Bao
           </h3>
           <blockquote className="card-text text-left">
             Thank you Thomas.
-            <br/>
-            [ add your message here ]
+            <br />I have learned a lot in this class. I really appreciate your effort to keep me in the class instead of giving up on the second week. I do not know how far I can go with coding as a career change, but it definitely helps me in the long term.
+
           </blockquote>
           <div className="icon-block text-center">
             <a
-              href="mailto:..."
+              href="mailto: laurabaoye@gmail.com"
               title="Direct Email"
             ><i className="fas fa-envelope-square"></i>
             </a>
             <a
-              href="https://github.com/..."
+              href="https://github.com/laurabaoye"
               target="_blank"
               rel="noopener noreferrer"
               title="GitHub"
             ><i className="fab fa-github"></i>
             </a>
             <a
-              href="https://www.linkedin.com/in/.../"
+              href="https://www.linkedin.com/in/laura-bao-designer/"
               target="_blank"
               rel="noopener noreferrer"
               title="LinkedIn"
             ><i className="fab fa-linkedin"></i>
             </a>
           </div>
+        </div>
       </div>
-    </div>
-  </main>
+    </main>
   );
 
 }
