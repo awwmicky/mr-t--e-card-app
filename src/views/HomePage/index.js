4594b4a893b6780a3aa8be20e3f41cec424bc446
awwmicky <michael_fred_alvarez@yahoo.com>
2020-04-29 15:08:36 -0700

    thank you card update

--- src/views/HomePage/index.js
+++ src/views/HomePage/index.js
@@ -9,28 +9,48 @@ export default function HomePage () {
 
   return (
     <main className="home-page component">
-      <span>— edit —</span>
-      <div className="e-card-app">
-        <img
-          src="./assets/images/profile-pic/mr-t-profile.png"
-          alt="mr-t-profile"
-          className="mr-t-profile"
-          // hidden
-        />
+      <div className="e-card-container">
+        <div className="e-card">
+          <div className="cover">
+            <div className="front-side">
+              {/* <h2>COVER-SIDE</h2> */}
+              <img
+                src="./assets/images/e-card-img/card-front-side.jpg"
+                alt="e-card-front"
+                className="e-card-front"
+              />
+            </div>
+            <div className="back-side">
+              {/* <h2>BACK-SIDE</h2> */}
+              <img
+                src="./assets/images/e-card-img/card-left-side.jpg"
+                alt="e-card-front"
+                className="e-card-front"
+              />
+            </div>
+          </div>
+          <div className="greeting">
+            {/* <h2>GREETING SIDE</h2> */}
+            <img
+              src="./assets/images/e-card-img/card-right-side.jpg"
+              alt="e-card-front"
+              className="e-card-front"
+            />
+          </div>
+        </div>
 
-        <img
-          src="./assets/images/other/Scan_20200412--1.jpg"
-          alt="e-card-front"
-          className="e-card-front"
-          hidden
-        />
+        <div className="easter-egg-t">
+          <img
+            src="./assets/images/profile-pic/mr-t-profile.png"
+            alt="mr-t-profile"
+            className="mr-t-profile"
+          />
+        </div>
 
-        <img
-          src="./assets/images/other/Scan_20200412--3.jpg"
-          alt="e-card-inside"
-          className="e-card-inside"
-          hidden
-        />
+        <div className="welcome-text">
+          <h2>From all of us, to you.</h2>
+          <h2>Keep flexin that code.</h2>
+        </div>
       </div>
     </main>
   );
