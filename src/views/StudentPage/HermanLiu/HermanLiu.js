5288f5efc0f17b1beced24b384e7fec086737e52
Herman Liu <hermanliu95@gmail.com>
2020-04-29 19:15:53 -0700

    herman changes

--- src/views/StudentPage/HermanLiu/HermanLiu.js
+++ src/views/StudentPage/HermanLiu/HermanLiu.js
@@ -14,49 +14,49 @@
 import React from 'react'
 import './HermanLiu.css'
 
-export default function HermanLiu () {
+export default function HermanLiu() {
 
   return (
-    <main className="d-flex align-items-center justify-content-center">
+    <main className="d-flex align-items-center justify-content-center herman-page">
       <div className="card">
         <img
           className="card-img"
-          src="./assets/images/profile-pic/_[your-profile-pic.img]_"
+          src="./assets/images/profile-pic/herman.JPG"
           alt="student-profile-pic"
         />
         <div className="card-body">
           <h3 className="card-title font-weight-bold text-center">
-            Your First & Last Name
+            Herman Liu
           </h3>
           <blockquote className="card-text text-left">
-            Thank you Thomas.
-            <br/>
-            [ add your message here ]
+            Thank you Thomas,
+            <br />
+            I really didn't know what I was getting myself into enrolling in this bootcamp. I didn't think I could had finished. Thanks to your teachings it motivated me to continue learning and striving for more. Keep on rocking dude! You're an inspiration.
           </blockquote>
           <div className="icon-block text-center">
             <a
-              href="mailto:..."
+              href="mailto:hermanliu95@gmail.com"
               title="Direct Email"
             ><i className="fas fa-envelope-square"></i>
             </a>
             <a
-              href="https://github.com/..."
-              target="_blank"
+              href="https://github.com/hermsicle"
+              target="https://github.com/hermsicle"
               rel="noopener noreferrer"
               title="GitHub"
             ><i className="fab fa-github"></i>
             </a>
             <a
-              href="https://www.linkedin.com/in/.../"
-              target="_blank"
+              href="https://www.linkedin.com/in/hermanliu95/"
+              target="https://www.linkedin.com/in/hermanliu95/"
               rel="noopener noreferrer"
               title="LinkedIn"
             ><i className="fab fa-linkedin"></i>
             </a>
           </div>
+        </div>
       </div>
-    </div>
-  </main>
+    </main>
   );
 
 }
